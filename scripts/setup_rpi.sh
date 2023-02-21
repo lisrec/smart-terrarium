70c3e9e841f4644a1dbc080298355c9c4157917c
lisrec <lisrec94@gmail.com>
2023-02-21 20:02:19 +0100

    working

--- scripts/setup_rpi.sh
+++ scripts/setup_rpi.sh
@@ -5,10 +5,27 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+# Install linux stuff
 # apt update
 # apt upgrade -y
+# apt install -y python3 python3-pip nginx htop curl wget
 
-# apt install -y python3 python3-pip
-apt install -y nginx
-
+# Install python libs
 pip3 install RPi.GPIO
+pip3 install flask
+
+# NVM
+touch ~/.bashrc
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install 16
+nvm install 18
+
+# build frontend
+cd frontend
+nvm use
+npm install
+npm run build
+cd ..
+
+pwd
